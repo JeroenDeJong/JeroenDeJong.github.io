7595e8b4b2480ea3943630fbb0770ef1b6f5e21a
Jeroen de Jong <jdjjong@icloud.com>
2022-11-04 18:43:01 +0100

    Re-write: completely start afresh

--- app/src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-let btn = document.getElementById("btn");
-
-btn.onmousemove = function (e: any) {
-  let rect = e.target.getBoundingClientRect();
-
-  let x = e.clientX - rect.left;
-  btn.style.setProperty("--x", x + "px");
-};
-  
