540fbb7b30d0dd969f5dbf393c385fdfb9ca6c9c
Jeroen de Jong <jdjjong@icloud.com>
2023-11-25 09:05:11 +0100

    FEAT: add textual clock to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,23 +18,34 @@ function Projects() {
     <Section title="Projects">
       <Container>
         <ExperienceContainer>
-          <>
-            <div>
-              <ExperienceImage src="/assets/JeroenTravel.png"/>
-            </div>
-            <div>
-              <ExperienceTitle as='a' rel="noopener noreferrer" href="http://www.jeroentravel.com">Jeroen Travel - My Personal Travel Tracker</ExperienceTitle>
-              <p>
-                I made this personal travel website out of a longing for a better experience during travel as well as the ability for a more detailed geographical representation of my travels.
-                I have developed an admin portal for this, which i can acces to upload gps tracks data as well as pictures. From the pictures uploaded, the GPS data will automaticallically be pulled out of the picture,
-                and these picture will then also be represented on the map. 
+          <div>
+            <ExperienceImage src="/assets/JeroenTravel.png"/>
+          </div>
+          <div>
+            <ExperienceTitle as='a' rel="noopener noreferrer" href="http://www.jeroentravel.com">Jeroen Travel - My Personal Travel Tracker</ExperienceTitle>
+            <p>
+              I made this personal travel website out of a longing for a better experience during travel as well as the ability for a more detailed geographical representation of my travels.
+              I have developed an admin portal for this, which i can acces to upload gps tracks data as well as pictures. From the pictures uploaded, the GPS data will automaticallically be pulled out of the picture,
+              and these picture will then also be represented on the map. 
 
-                Furthermore it is also possible to write stories on every entry. 
+              Furthermore it is also possible to write stories on every entry. 
 
-                This project is in active work in progress, so some features might still require improvements. 
-              </p>
-            </div>
-          </>
+              This project is in active work in progress, so some features might still require improvements. 
+            </p>
+          </div>
+        </ExperienceContainer>
+        <ExperienceContainer>
+          <div>
+            <ExperienceImage src="/assets/textual-clock.png"/>
+          </div>
+          <div>
+            <ExperienceTitle as='a' rel="noopener noreferrer" href="http://www.jeroenblog.com/textual-clock">Time through spoken words</ExperienceTitle>
+            <p>
+              At some point I was walking through the a city and saw a clock that sparked the idea of this project. This clock works for a few different languages
+              that I know. It makes use of an algorithm called rectangle packing to find the most optimal way through which the words are filled. I implemented a restricted
+              version of this however to ensure it always is readable from up to down. 
+            </p>
+          </div>
         </ExperienceContainer>
       </Container>
     </Section>
